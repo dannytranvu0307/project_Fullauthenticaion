2137a469446b16f349a8b1c0e214f9a951a4c91c
vu <quachvotinh005@gmail.com>
2024-07-17 14:57:39 +0900

    test deployment

---
+++ src/main/java/com/example/project_FullAuthentication/Entity/userEntity.java
@@ -0,0 +1,7 @@
+package com.example.project_FullAuthentication.Entity;
+import jakarta.persistence.Entity;
+import org.springframework.security.core.userdetails.UserDetails;
+@Entity
+public class userEntity implements UserDetails {
+
+}
